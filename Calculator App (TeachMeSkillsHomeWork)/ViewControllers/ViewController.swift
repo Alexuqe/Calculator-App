804c5ed28d0b9d0c6512f1caf8b53978345e91eb
alex <hordiah1@google.com>
2024-12-25 22:56:24 +0300

    Create final app

--- Calculator App (TeachMeSkillsHomeWork)/ViewControllers/ViewController.swift
+++ Calculator App (TeachMeSkillsHomeWork)/ViewControllers/ViewController.swift
@@ -8,30 +8,23 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    
+
         //MARK: - Calculator Properties
     private let calculator = CalculatorManager.shared
-    
+
         //MARK: - Layout Properties
     private let leadingConstraint: CGFloat = 20
     private let trailingConctraint: CGFloat = -20
     private let indent: CGFloat = -15
-    
+
         //MARK: - UI Labels
-    private lazy var operationLabel: UILabel = {
-        let label = getLabel(text: "0")
-        return label
-    }()
-    
-    private lazy var secondaryOperationLabel: UILabel = {
-        let label = getLabel(
-            text: "",
-            font: .systemFont(ofSize: 36, weight: .regular),
-            color: .gray
-        )
-        return label
-    }()
-    
+    private lazy var operationLabel: UILabel = getLabel(text: "0")
+
+    private lazy var secondaryOperationLabel: UILabel = getLabel(
+        text: "",
+        font: .systemFont(ofSize: 36, weight: .regular),
+        color: .gray)
+
         //MARK: - UI Buttons
     private let zeroButton = ButtonWithDropShadow().getButton(
         with: "0",
@@ -46,7 +39,7 @@ final class ViewController: UIViewController {
     private let numberSeven = ButtonWithDropShadow().getButton(with: "7")
     private let numberEight = ButtonWithDropShadow().getButton(with: "8")
     private let numberNine = ButtonWithDropShadow().getButton(with: "9")
-    
+
         //MARK: - Operation Buttons
     private let equallyButton = ButtonWithDropShadow().getButton(with: "=")
     private let plusButton = ButtonWithDropShadow().getButton(with: "+")
@@ -56,9 +49,9 @@ final class ViewController: UIViewController {
     private let percentButton = ButtonWithDropShadow().getButton(with: "%")
     private let togleNumberEquale = ButtonWithDropShadow().getButton(with: "+/-")
     private let dotButton = ButtonWithDropShadow().getButton(with: ".")
-    
+
     private let cancelButton = ButtonWithDropShadow().getButton(with: "C")
-    
+
         //MARK: - Stack Views
     private lazy var zeroRowButtons: [UIView] = [zeroButton, dotButton, equallyButton]
     private lazy var firstRowButtons: [UIView] = [numberOne, numberTwo, numberThree, plusButton]
@@ -70,7 +63,7 @@ final class ViewController: UIViewController {
         cancelButton, togleNumberEquale, percentButton, separationButton,
     ]
     private lazy var operationColumsLabels: [UIView] = [secondaryOperationLabel, operationLabel]
-    
+
     private lazy var zeroRowStackView = getStackView(distribution: .fill, subviews: zeroRowButtons)
     private lazy var firstRowStackView = getStackView(subviews: firstRowButtons)
     private lazy var secondRowStackView = getStackView(subviews: secondRowButtons)
@@ -83,7 +76,7 @@ final class ViewController: UIViewController {
         aligment: .trailing,
         subviews: operationColumsLabels
     )
-    
+
         //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -95,22 +88,22 @@ final class ViewController: UIViewController {
 
     //MARK: - Extension ViewController
 extension ViewController {
-    
+
         //MARK: - UI update
-    fileprivate func updateUI() {
+    private func updateUI() {
         configureNumbers()
         configureConstraints()
     }
-    
+
         //MARK: - UI Configuration
-    fileprivate func configureNumbers() {
+    private func configureNumbers() {
         addSubview(UIElement: operationsColumStackView)
         addSubview(UIElement: zeroRowStackView)
         addSubview(UIElement: firstRowStackView)
         addSubview(UIElement: secondRowStackView)
         addSubview(UIElement: thirdRowStackView)
         addSubview(UIElement: topRowStackView)
-        
+
         getSized(element: zeroButton, width: 175, height: 80)
         getSized(element: numberOne)
         getSized(element: numberTwo)
@@ -121,7 +114,7 @@ extension ViewController {
         getSized(element: numberSeven)
         getSized(element: numberEight)
         getSized(element: numberNine)
-        
+
         getSized(element: plusButton)
         getSized(element: dotButton)
         getSized(element: equallyButton)
@@ -130,55 +123,78 @@ extension ViewController {
         getSized(element: percentButton)
         getSized(element: cancelButton)
         getSized(element: togleNumberEquale)
-        
+
         setupButtonActions()
     }
-    
+
         //MARK: - UI Constraints
-    fileprivate func configureConstraints() {
-        NSLayoutConstraint.activate([
-            zeroRowStackView.bottomAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: trailingConctraint),
-            zeroRowStackView.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: leadingConstraint),
-            zeroRowStackView.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor, constant: trailingConctraint),
-            
-            firstRowStackView.bottomAnchor.constraint(
-                equalTo: zeroRowStackView.topAnchor, constant: indent),
-            firstRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
-            firstRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
-            
-            secondRowStackView.bottomAnchor.constraint(
-                equalTo: firstRowStackView.topAnchor, constant: indent),
-            secondRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
-            secondRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
-            
-            thirdRowStackView.bottomAnchor.constraint(
-                equalTo: secondRowStackView.topAnchor, constant: indent),
-            thirdRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
-            thirdRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
-            
-            topRowStackView.bottomAnchor.constraint(
-                equalTo: thirdRowStackView.topAnchor, constant: indent),
-            topRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
-            topRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
-            
-            operationsColumStackView.bottomAnchor.constraint(
-                equalTo: topRowStackView.topAnchor, constant: -37),
-            operationsColumStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
-            operationsColumStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
-            
-        ])
+    private func configureConstraints() {
+        NSLayoutConstraint.activate(
+            [
+                //Zero
+                zeroRowStackView.bottomAnchor.constraint(
+                    equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                    constant: trailingConctraint
+                ),
+                zeroRowStackView.leadingAnchor.constraint(
+                    equalTo: view.leadingAnchor,
+                    constant: leadingConstraint
+                ),
+                zeroRowStackView.trailingAnchor.constraint(
+                    equalTo: view.trailingAnchor,
+                    constant: trailingConctraint
+                ),
+
+                //First
+                firstRowStackView.bottomAnchor.constraint(
+                    equalTo: zeroRowStackView.topAnchor,
+                    constant: indent
+                ),
+                firstRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
+                firstRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
+
+                //Second
+                secondRowStackView.bottomAnchor.constraint(
+                    equalTo: firstRowStackView.topAnchor,
+                    constant: indent
+                ),
+                secondRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
+                secondRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
+
+                //Third
+                thirdRowStackView.bottomAnchor.constraint(
+                    equalTo: secondRowStackView.topAnchor,
+                    constant: indent
+                ),
+                thirdRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
+                thirdRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
+
+                //Top
+                topRowStackView.bottomAnchor.constraint(
+                    equalTo: thirdRowStackView.topAnchor,
+                    constant: indent
+                ),
+                topRowStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
+                topRowStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
+
+                //Operations
+                operationsColumStackView.bottomAnchor.constraint(
+                    equalTo: topRowStackView.topAnchor,
+                    constant: -37
+                ),
+                operationsColumStackView.leadingAnchor.constraint(equalTo: zeroRowStackView.leadingAnchor),
+                operationsColumStackView.trailingAnchor.constraint(equalTo: zeroRowStackView.trailingAnchor),
+            ]
+        )
     }
-    
+
         //MARK: - UI Helpers
-    fileprivate func addSubview(UIElement: UIView) {
+    private func addSubview(UIElement: UIView) {
         UIElement.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(UIElement)
     }
-    
-    fileprivate func getLabel(
+
+    private func getLabel(
         text: String,
         font: UIFont = .systemFont(ofSize: 72, weight: .bold),
         color: UIColor = .white
@@ -190,13 +206,12 @@ extension ViewController {
             $0.numberOfLines = 1
             $0.adjustsFontSizeToFitWidth = true
             $0.textAlignment = .right
-            
+
             return $0
-            
         }(UILabel())
     }
-    
-    fileprivate func getStackView(
+
+    private func getStackView(
         axis: NSLayoutConstraint.Axis = .horizontal,
         spacing: CGFloat = 15,
         distribution: UIStackView.Distribution = .fillEqually,
@@ -209,21 +224,21 @@ extension ViewController {
             stackView.distribution = distribution
             stackView.axis = axis
             subviews.forEach { stackView.addArrangedSubview($0) }
-            
+
             return stackView
-            
+
         }(UIStackView())
     }
-    
-    fileprivate func getSized(element: UIView, width: CGFloat = 80, height: CGFloat = 80) {
+
+    private func getSized(element: UIView, width: CGFloat = 80, height: CGFloat = 80) {
         NSLayoutConstraint.activate([
             element.heightAnchor.constraint(equalToConstant: height),
             element.widthAnchor.constraint(equalToConstant: width),
         ])
     }
-    
+
         //MARK: - Setup Button Actions
-    fileprivate func setupButtonActions() {
+    private func setupButtonActions() {
             // Numbers
         numberOne.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         numberTwo.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
@@ -235,21 +250,21 @@ extension ViewController {
         numberEight.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         numberNine.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         zeroButton.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
-        
+
             // Operations
         plusButton.addTarget(self, action: #selector(operationPressed(_:)), for: .touchUpInside)
         minusButton.addTarget(self, action: #selector(operationPressed(_:)), for: .touchUpInside)
         multiplyButton.addTarget(self, action: #selector(operationPressed(_:)), for: .touchUpInside)
         separationButton.addTarget(self, action: #selector(operationPressed(_:)), for: .touchUpInside)
         percentButton.addTarget(self, action: #selector(operationPressed(_:)), for: .touchUpInside)
-        
+
             // Special operations
         equallyButton.addTarget(self, action: #selector(equalPressed), for: .touchUpInside)
         cancelButton.addTarget(self, action: #selector(clearPressed), for: .touchUpInside)
         togleNumberEquale.addTarget(self, action: #selector(togglePressed), for: .touchUpInside)
         dotButton.addTarget(self, action: #selector(decimalPressed), for: .touchUpInside)
     }
-    
+
         //MARK: - Button Actions
     @objc private func numberPressed(_ sender: UIButton) {
         if let number = sender.titleLabel?.text {
@@ -259,7 +274,7 @@ extension ViewController {
             operationLabel.text = calculator.currentNumber
         }
     }
-    
+
     @objc private func operationPressed(_ sender: UIButton) {
         guard let symbol = sender.titleLabel?.text else { return }
         switch symbol {
@@ -270,36 +285,38 @@ extension ViewController {
             case "%": calculator.setOperation(.percent)
             default: break
         }
-        
+
         secondaryOperationLabel.text = calculator.previousNumber + " " + calculator.displayOperation
         operationLabel.text = calculator.currentNumber
     }
-    
+
     @objc private func equalPressed() {
         calculator.calculateResult()
         operationLabel.text = calculator.currentNumber
         secondaryOperationLabel.text = ""
     }
-    
+
     @objc private func clearPressed() {
         calculator.reset()
         operationLabel.text = calculator.currentNumber
         secondaryOperationLabel.text = ""
     }
-    
+
     @objc private func togglePressed() {
         calculator.toggleSign()
         operationLabel.text = calculator.currentNumber
     }
-    
+
     @objc private func decimalPressed() {
         calculator.inputDecimal()
         operationLabel.text = calculator.currentNumber
     }
-    
+
 }
 
-#Preview{
-    let view = ViewController()
-    view
-}
+//MARK: Perview
+
+//#Preview{
+//    let view = ViewController()
+//    view
+//}
